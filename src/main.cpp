910c07ea5aa0f27a8cb5ff7d3102674bced385d1
emilia-szymanska <emiliaszym@gmail.com>
2020-05-16 15:40:52 +0200

    DFS with finding path and order of visiting

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,7 @@
 int main()
 {
 
-	vector<pair<unsigned int, char> > pairs;
-	vector<pair<unsigned int, unsigned int> > check;
-	unsigned int boardSize;
+	vector<pair<unsigned int, char> > pairs;						//putting figures on the board
 	unsigned int RookID = 1;
 	unsigned int KingID = 2;
 	unsigned int KnightID = 3;
@@ -18,52 +16,52 @@ int main()
 	pairs[1] = {KingID, 'H'};	//king
 	pairs[2] = {KnightID, 'W'};	//knight
 
-
-	Board board = Board(pairs);
-	boardSize = board.returnSize();
-	vector<pair<unsigned int, unsigned int> > trial;
-	trial = board.boardToVectors();
+	Board board = Board(pairs);								//initializing board
 	
-	unsigned int NoE = 25;
-	
-	GraphList grafik = GraphList(NoE);
-	
-	for(unsigned int i = 0; i < trial.size(); i++)
-	{
-		grafik.insertEdge(trial[i].first, trial[i].second, 1);
-	}
-	
-
-	vector<int> visited;
-	visited.resize(25);
-       	vector<unsigned int> order;
-       	DFS(grafik, 22, visited, order);
-	check = board.knightsCheck();
+	unsigned int boardSize = board.returnSize();						//useful variables
+	unsigned int KnightPosition = board.KnightsPosition();
+	unsigned int NoE = boardSize * boardSize;
+	vector<pair<unsigned int, unsigned int> > check = board.knightsCheck();
 	vector<unsigned int> checkFields;
-
 	for(unsigned int i = 0; i < check.size(); i++)
 	{
 		checkFields.push_back( check[i].first * boardSize + check[i].second);
 	}
+	
+	vector<pair<unsigned int, unsigned int> > listOfVertices = board.boardToVectors();	//board to graph
+	GraphList graph = GraphList(NoE);
+	for(unsigned int i = 0; i < listOfVertices.size(); i++)
+	{
+		graph.insertEdge(listOfVertices[i].first, listOfVertices[i].second, 1);
+	}
+	
+
+	vector<int> visited;									//DFS
+	visited.resize(NoE);
+     	vector<unsigned int> order;
+	vector<vector<char> > path;
+	path.resize(checkFields.size());
+
+	DFS(graph, KnightPosition, visited, order);
+	
+	vector<char> visitingOrder = makeOrder(order, NoE);					//visiing order
+	cout << "Visiting order: ";
+
+	for(unsigned int i = 0; i < visitingOrder.size(); i++)
+	{
+		cout << visitingOrder[i] << " ";
+	}
+	cout << endl;
+
+	cout << "Paths:" << endl;								//paths to check
 
 	for(unsigned int i = 0; i < checkFields.size(); i++)
 	{
-		cout << checkFields[i] << " ";
-	}
-	cout << endl;
-	
-
-/*	vector<char> ordered = makeOrder(order, 25);
-
-	for(unsigned int i = 0; i < ordered.size(); i++)
-		cout << ordered[i] << " ";
-	cout << endl;
-
-	vector<char> path = makePath(order, 6, 25);
-
-	for(unsigned int i = 0; i < path.size(); i++)
-		cout << path[i] << " ";
-	cout << endl;
-*/	
+		path[i] = makePath(order, checkFields[i], NoE); 
+		for(unsigned int j = 0; j < path[i].size(); j++)
+			cout << path[i][j] << " ";
+		cout << endl;	
+	}	
+		
 	return 0;
 }
